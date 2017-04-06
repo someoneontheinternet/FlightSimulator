d732950ee3a15d88fd39a92ddfabee9d55679902
someoneontheinternet <henryjiang0x3639@gmail.com>
2017-04-06 11:48:45 -0700

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import org.lwjgl.opengl.Display;
+
+import models.RawModel;
+import models.TexturedModel;
+import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.Renderer;
+import shaders.StaticShader;
+import texture.ModelTexture;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		DisplayManager.createDisplay();
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		StaticShader shader = new StaticShader();
+
+		float[] vertices = { -0.5f, 0.5f, 0, // V0
+				-0.5f, -0.5f, 0, // V1
+				0.5f, -0.5f, 0, // V2
+				0.5f, 0.5f, 0 // V3
+		};
+
+		int[] indices = { 0, 1, 3, // Top left triangle (V0,V1,V3)
+				3, 1, 2 // Bottom right triangle (V3,V1,V2)
+		};
+
+		float[] textureCoords = {
+				0,0, //V0
+				0,1, //V1
+				1,1, //V2
+				1,0  //V3
+		};
+		
+		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("testTexture"));
+		TexturedModel texturedModel = new TexturedModel(model, texture);
+		
+		while (!Display.isCloseRequested()) {
+			// game logic
+			renderer.prepare();
+			shader.start();
+			renderer.render(texturedModel);
+			shader.stop();
+			DisplayManager.updateDisplay();
+		}
+
+		shader.cleanUp();
+		loader.cleanUp();
+		DisplayManager.closeDisplay();
+		System.exit(0);
+	}
+}
